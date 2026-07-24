10ee10560fb1e884c5bc399b30e78ecc33bea79c
MilanAlgama <milanharsha28@gmail.com>
2026-07-24 15:47:06 +0530

    Test run

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Milan-Algama/",
+  base: "/Portfolio-BackendConnection/",
   server: {
     proxy: {
       "/projects": "http://localhost:5001",
